993f01b367dd9494254a77e30641cdef0ab77361
rkenmi <rkenmi@gmail.com>
2016-05-06 00:51:49 -0700

    fixed clipping on charged beam sprites

--- player_beam_charged.py
+++ player_beam_charged.py
@@ -21,9 +21,22 @@ class ChargedBeam(Beam):
         self.sound = pygame.mixer.Sound(file="sounds/player_wpn2_shoot.ogg")
         self.charge_timer = 0
 
-        # Load image
-        self.image = pygame.image.load("sprites/black.gif").convert()
-        self.charge_image = pygame.image.load("sprites/player_wpn2_charge1.gif").convert()
+        # Load images
+        self.charge_images, self.shoot_images = [], []
+        for i in range (1, 8):
+            self.charge_images.append(pygame.image.load("sprites/player_wpn2_charge"+str(i)+".gif").convert())
+            if i < 6:
+                self.shoot_images.append(
+                    (
+                    pygame.image.load("sprites/player_wpn2_shoot"+str(i)+"a.gif").convert(),
+                    pygame.image.load("sprites/player_wpn2_shoot"+str(i)+"b.gif").convert()
+                     )
+                )
+
+        self.image = pygame.image.load("sprites/black.gif").convert() # temporary sprite
+
+        self.charge_image = self.charge_images[0]
+        #self.image = pygame.image.load("sprites/player_wpn2_shoot5a.gif").convert() # temporary sprite
 
         # Set the color that should be transparent
         self.image.set_colorkey(pygame.Color(0, 0, 0))
@@ -43,6 +56,7 @@ class ChargedBeam(Beam):
         self.shot_ready = False
 
 
+
     def draw(self, SCREEN):
         """ Draws to screen
 
@@ -52,22 +66,22 @@ class ChargedBeam(Beam):
         if self.charging:
             if not self.shot_ready:
                 self.charge_sound.play(-1)
-               #self.charge_sound.play()
 
             self.charge_timer += 1
             self.shot_ready = True
+
             if 0 <= self.charge_timer < 4:
-                self.charge_image = pygame.image.load("sprites/player_wpn2_charge2.gif").convert()
+                self.charge_image = self.charge_images[1]
             elif 4 < self.charge_timer < 8:
-                self.charge_image = pygame.image.load("sprites/player_wpn2_charge3.gif").convert()
+                self.charge_image = self.charge_images[2]
             elif 8 < self.charge_timer < 12:
-                self.charge_image = pygame.image.load("sprites/player_wpn2_charge4.gif").convert()
+                self.charge_image = self.charge_images[3]
             elif 12 < self.charge_timer < 16:
-                self.charge_image = pygame.image.load("sprites/player_wpn2_charge5.gif").convert()
+                self.charge_image = self.charge_images[4]
             elif 16 < self.charge_timer < 20:
-                self.charge_image = pygame.image.load("sprites/player_wpn2_charge6.gif").convert()
+                self.charge_image = self.charge_images[5]
             elif 20 < self.charge_timer < 24:
-                self.charge_image = pygame.image.load("sprites/player_wpn2_charge7.gif").convert()
+                self.charge_image = self.charge_images[6]
                 if self.charge_level < 6:
                     self.charge_level += 1
 
@@ -76,38 +90,38 @@ class ChargedBeam(Beam):
             self.charge_image.set_colorkey(pygame.Color(0, 0, 0))
 
         else: # key is let go, or beam is about to shoot
-
+            self.charge_image = None
             self.animation_loop += 1
             if self.charge_level == 1:
                 if self.animation_loop % 3 == 0:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot1b.gif").convert()
+                    self.image = self.shoot_images[0][1]
                 else:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot1a.gif").convert()
+                    self.image = self.shoot_images[0][0]
             elif self.charge_level == 2:
                 if self.animation_loop % 3 == 0:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot2b.gif").convert()
+                    self.image = self.shoot_images[1][1]
                 else:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot2a.gif").convert()
+                    self.image = self.shoot_images[1][0]
             elif self.charge_level == 3:
                 if self.animation_loop % 3 == 0:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot3b.gif").convert()
+                    self.image = self.shoot_images[2][1]
                 else:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot3a.gif").convert()
+                    self.image = self.shoot_images[2][0]
             elif self.charge_level == 4:
                 if self.animation_loop % 3 == 0:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot4b.gif").convert()
+                    self.image = self.shoot_images[2][1]
                 else:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot4a.gif").convert()
+                    self.image = self.shoot_images[2][0]
             elif self.charge_level == 5:
                 if self.animation_loop % 3 == 0:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot4b.gif").convert()
+                    self.image = self.shoot_images[3][1]
                 else:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot4a.gif").convert()
+                    self.image = self.shoot_images[3][0]
             elif self.charge_level == 6:
                 if self.animation_loop % 3 == 0:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot5b.gif").convert()
+                    self.image = self.shoot_images[4][1]
                 else:
-                    self.image = pygame.image.load("sprites/player_wpn2_shoot5a.gif").convert()
+                    self.image = self.shoot_images[4][0]
 
             if self.shot_ready:
                 self.charge_sound.stop()
@@ -126,31 +140,9 @@ class ChargedBeam(Beam):
                 elif self.charge_level == 6:
                     self.rect.y -= 15
 
+            x, y = self.rect.x, self.rect.y
+            self.rect = self.image.get_rect() # fixes clipping issue with charged beam sprites
+            self.rect.x, self.rect.y = x, y
+
             SCREEN.blit(self.image, (self.rect.x, self.rect.y))
             self.image.set_colorkey(pygame.Color(0, 0, 0))
-
-
-    def bounce(self):
-        """ Bounces the ball by inverting the angles.
-        """
-        if self.hit is False: # sort of helps with collision/stuck issues when balls hit each other
-            magnitude = math.sqrt(math.pow(self.vy, 2) + math.pow(self.vx, 2))
-            theta = math.atan2(self.vy, self.vx) # get velocity direction
-            theta += math.pi/2 # flip the velocity direction
-            self.vx = math.cos(theta) * magnitude
-            self.vy = math.sin(theta) * magnitude
-
-        self.hit = not self.hit
-
-
-    def move(self, SCREEN):
-        """ Player 1 beam (lvl 1) moves only in the +x direction
-
-        Arguments:
-            x (int): x coord to move
-            y (int): y coord to move
-        """
-
-        self.rect.x += self.vx
-        if self.rect.x > SCREEN.get_width():
-            self.out_of_screen = True
